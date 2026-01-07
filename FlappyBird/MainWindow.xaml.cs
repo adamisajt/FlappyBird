d2092cc2c13c6e658469db4f5602e3c1596657cd
aczee <aczee@ADAM>
2026-01-07 10:20:05 +0100

    Add project files.

---
+++ FlappyBird/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace FlappyBird
+{
+    public partial class MainWindow : Window
+    {
+        readonly DispatcherTimer _ido = new DispatcherTimer();
+
+        double _Y;          
+        double velocity;      
+        const double gravitacio = 0.5; 
+        const double ugrassebesseg = -8; 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _Y = 150;
+            Canvas.SetLeft(Bird, 150);
+            Canvas.SetTop(Bird, _Y);
+
+            _ido.Interval = TimeSpan.FromMilliseconds(16); 
+            _ido.Tick += GameLoop;
+            _ido.Start();
+        }
+
+        void GameLoop(object? sender, EventArgs e)
+        {
+            velocity += gravitacio;
+
+            _Y += velocity;
+
+            double minY = 0;
+            double maxY = GameCanvas.ActualHeight - Bird.ActualHeight;
+            if (maxY <= 0) maxY = 400; 
+
+            if (_Y < minY)
+            {
+                _Y = minY;
+                velocity = 0;
+            }
+            else if (_Y > maxY)
+            {
+                _Y = maxY;
+                velocity = 0;
+            }
+
+            Canvas.SetTop(Bird, _Y);
+        }
+
+        void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                velocity = ugrassebesseg; 
+            }
+        }
+    }
+}
